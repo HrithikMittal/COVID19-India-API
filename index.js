584e52eb644f32f251c4ca67b4fe90336733926e
HrithikMittal <adhikanshmittal@gmail.com>
2020-03-29 13:29:31 +0530

    connection to database

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 var express = require("express");
 var dotenv = require("dotenv").config();
 var app = express();
+var mongoose = require("mongoose");
 
-var port = process.env.PORT;
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  })
+  .then(() => {
+    console.log("Database connected Successfully");
+  })
+  .catch(err => {
+    console.log("Error is connection to DB " + err.message);
+  });
 
 app.get("/", (req, res) => {
   res.send("Welcome to this API");
 });
 
-app.listen(port, () => {
-  console.log(`PORT is listening on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`PORT is listening on port ${process.env.PORT}`);
 });
