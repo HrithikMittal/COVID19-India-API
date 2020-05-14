8348ca1606b1d145c4a7ecad92b864dad859926f
Restyled.io <commits@restyled.io>
2020-05-14 17:37:23 +0000

    Restyled by clang-format

--- index.js
+++ index.js
@@ -9,26 +9,18 @@ var Route = require("./routes/routes.js");
 var data = require("./home.json");
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-  })
-  .then(() => {
-    console.log("Database connected Successfully");
-  })
-  .catch(err => {
-    console.log("Error is connection to DB " + err.message);
-  });
+    .connect(process.env.MONGO_URI,
+             {useUnifiedTopology : true, useNewUrlParser : true})
+    .then(() => { console.log("Database connected Successfully"); })
+    .catch(err => { console.log("Error is connection to DB " + err.message); });
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
 app.use("/", Route);
 
-app.get("/", (req, res) => {
-  res.send(data);
-});
+app.get("/", (req, res) => { res.send(data); });
 
-app.listen(process.env.PORT, () => {
-  console.log(`PORT is listening on port ${process.env.PORT}`);
-});
+app.listen(
+    process.env.PORT,
+    () => { console.log(`PORT is listening on port ${process.env.PORT}`); });
--- routes/routes.js
+++ routes/routes.js
@@ -4,55 +4,44 @@ var State = require("../Models/state");
 
 router.get("/summary", (req, res) => {
   State.find()
-    .then(data => {
-      var confirmed = 0;
-      var death = 0;
-      var cured = 0;
-      var total = 0;
-      data.map(d => {
-        confirmed = confirmed + parseInt(d["confirmed"]);
-        death = death + parseInt(d["death"]);
-        cured = cured + parseInt(d["cured"]);
-      });
+      .then(data => {
+        var confirmed = 0;
+        var death = 0;
+        var cured = 0;
+        var total = 0;
+        data.map(d => {
+          confirmed = confirmed + parseInt(d["confirmed"]);
+          death = death + parseInt(d["death"]);
+          cured = cured + parseInt(d["cured"]);
+        });
 
-      total = confirmed + death + cured;
-      res.json({
-        "Total Confirmed cases": confirmed,
-        "Cured/Discharged/Migrated": cured,
-        Death: death,
-        "Total Cases": total
-      });
-    })
-    .catch(err => {
-      console.log("Error is ", err.message);
-    });
+        total = confirmed + death + cured;
+        res.json({
+          "Total Confirmed cases" : confirmed,
+          "Cured/Discharged/Migrated" : cured,
+          Death : death,
+          "Total Cases" : total
+        });
+      })
+      .catch(err => { console.log("Error is ", err.message); });
 });
 
 router.get("/states", (req, res) => {
   State.find()
-    .then(data => {
-      res.json({ state: data });
-    })
-    .catch(err => {
-      console.log("Error is ", err.message);
-    });
+      .then(data => { res.json({state : data}); })
+      .catch(err => { console.log("Error is ", err.message); });
 });
 
 router.get("/state/:statename", (req, res) => {
   var stateid = req.url.split("/");
   stateid = stateid[2].replace(/%20/g, " ");
   console.log(stateid);
-  State.find({ name: stateid })
-    .then(data => {
-      res.json({ data });
-    })
-    .catch(err => {
-      console.log("Error is ", err.message);
-    });
+  State.find({name : stateid})
+      .then(data => { res.json({data}); })
+      .catch(err => { console.log("Error is ", err.message); });
 });
 
-router.get("/latest", (req, res) => {
-  res.json({ message: "Latest Data Updated" });
-});
+router.get("/latest",
+           (req, res) => { res.json({message : "Latest Data Updated"}); });
 
 module.exports = router;
